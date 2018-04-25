0cc1a73778f351dc6a8709f8c94d753860b26f64
xuexue <1047504737@qq.com>
2018-04-25 20:05:42 +0800

    practice1 and practice2

--- src/main/java/practice2/Receipt.java
+++ src/main/java/practice2/Receipt.java
@@ -12,25 +12,27 @@ public class Receipt {
 
     private BigDecimal tax;
 
+    /**
+     * 计算并返回总价
+     * @param products 商品列表 （商品编码，价格，折扣）
+     * @param items 订单 （商品编码，数量）
+     * @return
+     */
     public double CalculateGrandTotal(List<Product> products, List<OrderItem> items) {
+        //items价格
         BigDecimal subTotal = calculateSubtotal(products, items);
-
-        for (Product product : products) {
-            OrderItem curItem = findOrderItemByProduct(items, product);
-
-            BigDecimal reducedPrice = product.getPrice()
-                    .multiply(product.getDiscountRate())
-                    .multiply(new BigDecimal(curItem.getCount()));
-
-            subTotal = subTotal.subtract(reducedPrice);
-        }
+        //税
         BigDecimal taxTotal = subTotal.multiply(tax);
         BigDecimal grandTotal = subTotal.add(taxTotal);
-
         return grandTotal.setScale(2, BigDecimal.ROUND_HALF_UP).doubleValue();
     }
 
-
+    /**
+     * 根据product获得orderItem
+     * @param items
+     * @param product
+     * @return
+     */
     private OrderItem findOrderItemByProduct(List<OrderItem> items, Product product) {
         OrderItem curItem = null;
         for (OrderItem item : items) {
@@ -42,13 +44,30 @@ public class Receipt {
         return curItem;
     }
 
+    /**
+     * 获得所有items 折扣前价格 ----> 折扣后价格
+     * @param products
+     * @param items
+     * @return
+     */
     private BigDecimal calculateSubtotal(List<Product> products, List<OrderItem> items) {
         BigDecimal subTotal = new BigDecimal(0);
         for (Product product : products) {
             OrderItem item = findOrderItemByProduct(items, product);
-            BigDecimal itemTotal = product.getPrice().multiply(new BigDecimal(item.getCount()));
-            subTotal = subTotal.add(itemTotal);
+            BigDecimal itemTotal = getItemTotal(product,item);
+            //折扣
+            BigDecimal reducedPrice = getItemTotal(product,item).multiply(product.getDiscountRate());
+            subTotal = subTotal.add(itemTotal).subtract(reducedPrice);
         }
         return subTotal;
+    }
+    /**
+     * 获得一个 item 的价钱，单价 * 数量
+     * @param product
+     * @param item
+     * @return
+     */
+    private BigDecimal getItemTotal(Product product,OrderItem item){
+        return product.getPrice().multiply(new BigDecimal(item.getCount()));
     }
 }
